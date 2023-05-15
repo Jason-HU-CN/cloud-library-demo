501bfcabe375e073eb1709ea9ecbbb347b7c4484
jason <939392848>
2023-05-15 11:33:21 +0800

    fist

--- src/main/java/com/design/config/SpringMvcConfig.java
+++ src/main/java/com/design/config/SpringMvcConfig.java
@@ -1,6 +1,27 @@
 package com.design.config;
 
-/*Spring MVC 相关的配置类
-* */
-public class SpringMvcConfig {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan({"com.design.controller"})
+@EnableWebMvc
+public class SpringMvcConfig implements WebMvcConfigurer {
+    /*
+    * 开启静态资源访问
+    * 类似在Spring MVC的配置文件中设置<mvc:default-servlet-handler/>元素
+    * */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/admin/",".jsp");
+    }
 }
